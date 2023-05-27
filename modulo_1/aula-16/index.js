21d02601ca00cfd2b8aaf0d7313fb10cacc33386
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-27 15:30:01 -0300

    exercicio 2

---
+++ modulo_1/aula-16/index.js
@@ -0,0 +1,7 @@
+let idade = prompt("Digite a sua idade:")
+
+if (idade > 18) { alert("Você é maior de idade")
+
+} else {
+    alert("Você é menor de idade")
+}
---
+++ modulo_1/exercicio-2/teste_de_velocidade/index.js
@@ -0,0 +1,4 @@
+/*
+# [Escreva um programa em javascript que permita inserir o nome e a velocidade de dois veículos e exiba na tela uma mensagem dizendo qual dos dois é mais rápido (ou que as velocidades são iguais se este for o caso)](https://www.mindmeister.com/map/2783164155)
+
+*/
