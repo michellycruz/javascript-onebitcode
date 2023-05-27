9bf46b2192e2905277c3c925b1973324db811849
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-27 13:01:13 -0300

    exercicio 1

---
+++ modulo_1/aula-10/index.js
@@ -0,0 +1,11 @@
+let nome = "Michelly"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome:")
+//let confirmacao = window.confirm("Você confirma essa ação?")
+
---
+++ modulo_1/exercicio-1/index.js
@@ -0,0 +1,16 @@
+/*
+# [Escreva um programa em javascript que permita salvar informações de um recruta. As informações a serem salvas são: - o primeiro nome - o sobrenome - o campo de estudo - o ano de nascimento Depois o programa deve exibir o nome completo do recruta, seu campo de estudo e sua idade (apenas baseada no ano de nascimento).](https://www.mindmeister.com/map/2783164111)
+ */
+
+let nome = prompt("Me informe o seu nome e sobrenome:")
+let estudo = prompt("Me informe o seu campo de estudo:")
+let ano = prompt("Me informe o ano que você nasceu:")
+
+
+
+const anoAtual = new Date().getFullYear()
+
+let idade = (anoAtual - ano)
+
+
+window.alert(nome + " " + estudo + " " + idade)
