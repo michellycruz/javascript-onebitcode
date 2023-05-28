27a4d084a4c643c5bd25945fa48dceb0544bad8f
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-28 12:42:37 -0300

    estrutura de repetição: while

---
+++ modulo_1/aula-22/while.js
@@ -0,0 +1,10 @@
+let velocidade = 80
+
+
+    while (velocidade > 0){
+        alert("o carro está a " + velocidade + " km/h")
+        velocidade -= 20
+        alert("Diminuindo 20km")
+    }
+
+    alert("O carro parou")
