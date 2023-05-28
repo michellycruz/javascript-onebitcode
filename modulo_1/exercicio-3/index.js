28549f2c94a021ffae2311a190c1863890e93cf9
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-28 11:44:15 -0300

    Update index.js

--- modulo_1/exercicio-3/index.js
+++ modulo_1/exercicio-3/index.js
@@ -3,47 +3,28 @@
 
 */
 
-    let metros = prompt("Defina o valor em metros que deseja converter")
-    let resultado = prompt("Para qual unidade de medida gostaria de converter? \na) milímetro (mm) \nb) centímetro (cm) \nc) decímetro (dm) \nd) decâmetro (dam) \ne) hectômetro (hm) \nf) quilômetro (km)")
-    let resultadoNumerico = parseFloat(metros)
+    const metros = parseFloat(prompt("Defina o valor em metros que deseja converter:"));
+    const unidade = prompt("Para qual unidade de medida gostaria de converter? \na) milímetro (mm) \nb) centímetro (cm) \nc) decímetro (dm) \nd) decâmetro (dam) \ne) hectômetro (hm) \nf) quilômetro (km)")
 
-    switch (resultado){
+    switch (unidade){
         case "a":
-            resultado = resultadoNumerico * 1000
-        alert("O valor de " + metros + "m em milímetros é " + resultado + "mm")
-
-        break
-
-        case "b":
-            resultado = resultadoNumerico * 100
-        alert("O valor de " + metros + "m em centímetros é " + resultado + "cm")
-
-        break
-
+            alert("O valor de " + metros + "m em milímetros é: " + metros * 1000 + "mm")
+            break
+        case "b":    
+            alert("O valor de " + metros + "m em centímetros é: " + metros * 100 + "cm")
+            break
         case "c":
-            resultado = resultadoNumerico * 10
-        alert("O valor de " + metros + "m em decímetros é " + resultado + "dm")
-
-        break
-
+            alert("O valor de " + metros + "m em decímetros é: " + metros * 10 + "dm")
+            break
         case "d":
-            resultado = resultadoNumerico * 0.1
-        alert("O valor de " + metros + "m em decâmetros é " + resultado + "dam")
-
-        break
-
+            alert("O valor de " + metros + "m em decâmetros é: " + metros * 0.1 + "dam")
+            break
         case "e":
-            resultado = resultadoNumerico * 0.01
-        alert("O valor de " + metros + "m em hectômetros é " + resultado + "hm")
-
-        break
-
+            alert("O valor de " + metros + "m em hectômetros é: " + metros * 0.01 + "hm")
+            break
         case "f":
-            resultado = resultadoNumerico * 0.001
-        alert("O valor de " + metros + "m em quilômetros é " + resultado + "km")
-
-        break
-
+            alert("O valor de " + metros + "m em quilômetros é: " + metros * 0.001 + "km")
+            break
         default:
         alert("O valor é inválido")
     }
