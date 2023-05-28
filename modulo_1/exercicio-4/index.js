c6b51e8b9dcbf6f2763d71e2e8c30dafa4250673
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-28 17:18:36 -0300

    exercicio 4 while

---
+++ modulo_1/exercicio-4/index.js
@@ -0,0 +1,19 @@
+/*
+# [Escreva um programa em javascript que peça o nome de um turista e então pergunte se ele já visitou alguma cidade. Caso a resposta seja sim, pergunte o nome da cidade e o armazene em uma variável, e então continue perguntando se o turista visitou alguma outra cidade até que a resposta seja não. No fim, o programa deve exibir o nome do turista, quantas e quais cidades ele visitou.](https://www.mindmeister.com/map/2783164177)
+*/
+
+
+const turista = prompt("Qual o nome do turista?")
+let visitou = prompt("Ele já visitou alguma cidade? sim/não")
+let cidade = prompt("Qual o nome da cidade?")
+
+while (visitou == "sim") {
+    visitou = prompt("Ele visitou mais alguma? sim/não")
+    cidade = prompt("Me informe o nome da cidade:")
+    if( visitou == "não"){
+        break
+    }
+
+}
+
+alert(turista + " visitou " + cidade)
