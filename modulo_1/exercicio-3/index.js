5333946ef2457d950a100ba6577b1e721f3b71ef
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-28 10:15:52 -0300

    conversor de medidas

--- modulo_1/exercicio-3/index.js
+++ modulo_1/exercicio-3/index.js
@@ -3,3 +3,47 @@
 
 */
 
+    const metros = prompt("Defina o valor em metros que deseja converter")
+    const resultado = prompt("Para qual unidade de medida gostaria de converter? \na) milímetro (mm) \nb) centímetro (cm) \nc) decímetro (dm) \n d) decâmetro (dam) \ne) hectômetro (hm) \nf) quilômetro (km)")
+    const resultadoNumerico = parseFloat(metros)
+
+    switch (resultadoNumerico){
+        case "a":
+            (resultado = (metros * 1000))
+        alert("O valor de " + metros + "m em milímetros é " + resultado + "mm")
+
+        break
+
+        case "b":
+            (resultado = (metros * 100))
+        alert("O valor de " + metros + "m em centímetros é " + resultado + "cm")
+
+        break
+
+        case "c":
+            (resultado = (metros * 10))
+        alert("O valor de " + metros + "m em decímetros é " + resultado + "dm")
+
+        break
+
+        case "a":
+            (resultado = metros * 0.1)
+        alert("O valor de " + metros + "m em decâmetros é" + resultado + "dam")
+
+        break
+
+        case "a":
+            (resultado = metros * 0.01)
+        alert("O valor de " + metros + "m em hectômetros é" + resultado + "hm")
+
+        break
+
+        case "a":
+            (resultado = metros * 0.001)
+        alert("O valor de " + metros + "m em quilômetros é" + resultado + "km")
+
+        break
+
+        default:
+        alert("O valor é inválido")
+    }
