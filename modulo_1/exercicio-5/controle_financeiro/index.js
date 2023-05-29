aa974bbca5b4a661e43a34d3186c13620055dee9
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-29 11:09:13 -0300

    descrição dos exercícios

---
+++ modulo_1/exercicio-5/controle_financeiro/index.js
@@ -0,0 +1,4 @@
+/*
+# [Escreva um programa em javascript que comece perguntando pela quantidade inicial de dinheiro disponível e então mostre na tela essa quantidade juntamente com as opções de adicionar e remover dinheiro e uma opção de sair. Ao clicar na opção de adicionar dinheiro o programa deve perguntar pela quantidade a ser adicionada, somar esse valor com a quantidade inicial e então mostrar novamente o menu com a quantidade de dinheiro e as opções. A opção de remover dinheiro deve fazer o mesmo, porém subtraindo o valor. A opção de sair deve encerrar o programa.](https://www.mindmeister.com/map/2783164189)
+
+*/
---
+++ modulo_1/exercicio-5/menu_interativo/index.js
@@ -0,0 +1,4 @@
+/*
+# [Escreva um programa em javascript que simule um menu interativo com 5 opções diferentes. O programa deverá iniciar mostrando as 5 opções disponíveis, as quatro primeiras não precisam de nenhuma funcionalidade específica, a última opção deve ser a opção “Encerrar”. Ao clicar em alguma das quatro primeiras opções uma mensagem deve ser exibida na tela indicando qual foi a opção escolhida e depois o programa deve exibir novamente o menu com as 5 opções. Isso deve continuar acontecendo até que o usuário escolha a opção “Encerrar”. Ao escolhe-la uma mensagem dizendo que o sistema está sendo encerrado deve ser exibida na tela e então o programa é finalizado.](https://www.mindmeister.com/map/2783164187)
+
+*/
