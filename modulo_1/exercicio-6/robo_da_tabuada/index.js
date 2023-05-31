5ab3a96b4cc53ba2db9739a88b8da1741fb4e799
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-31 00:52:52 -0300

    exercício 6

---
+++ modulo_1/exercicio-6/robo_da_tabuada/index.js
@@ -0,0 +1,2 @@
+//# [Escreva um programa em javascript que seja capaz de calcular a tabuada de um determinado número (de 1 a 20). O programa deve começar pedindo o número a ser usado nos cálculos e então salvar todos os resultados de multiplicações de 1 a 20 usando o número informado dentro de uma variável do tipo string. Depois o programa deve exibir esses resultados e finalizar.](https://www.mindmeister.com/map/2783164201)
+
