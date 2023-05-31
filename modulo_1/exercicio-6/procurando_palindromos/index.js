7c299df0d62d43b26a44ddb52ac7c7ccc1a23036
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-31 15:22:41 -0300

    Update index.js

--- modulo_1/exercicio-6/procurando_palindromos/index.js
+++ modulo_1/exercicio-6/procurando_palindromos/index.js
@@ -1,2 +1,7 @@
-//# [Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.](https://www.mindmeister.com/map/2783164203)
+/*Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.
 
+
+*/
+
+let palavra = prompt("-----VERIFICADOR DE PALINDROMOS-----\n" + "\nQual palavra você gostaria de verificar se é um palindromo ou não?")
+let invertida = ""
