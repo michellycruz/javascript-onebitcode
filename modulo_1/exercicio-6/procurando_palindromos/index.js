2e4b53a96797896a764fcb2861878fdbba4f6238
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-31 15:26:43 -0300

    instrução para fazer o exercício

--- modulo_1/exercicio-6/procurando_palindromos/index.js
+++ modulo_1/exercicio-6/procurando_palindromos/index.js
@@ -1,5 +1,14 @@
 /*Escreva um programa em javascript que seja capaz de identificar se uma palavra é um palíndromo. Um palíndromo é uma palavra que lida de trás para frente possui as mesmas letras na mesma ordem. O programa deve iniciar pedindo que seja informada uma palavra e então deve exibir uma mensagem dizendo se aquela palavra é ou não um palíndromo. Caso não seja um palíndromo, o programa deve mostrar a palavra lida da esquerda para direita e da direita para esquerda.
 
+1. Solicite uma palavra ao usuário utilizando a função **`prompt()`** e armazene-a em uma variável.
+2. Crie uma variável para armazenar a palavra invertida. Você pode inicializá-la como uma string vazia.
+3. Utilize um loop **`for`** para iterar sobre cada letra da palavra digitada. Lembre-se de usar o método **`length`** da string para determinar o número de caracteres.
+4. Dentro do loop, acesse cada letra da palavra e adicione-a à variável da palavra invertida, de trás para frente.
+5. Após o loop, verifique se a palavra digitada é igual à palavra invertida. Você pode usar uma estrutura condicional **`if`** para isso.
+6. Se a palavra for um palíndromo, exiba uma mensagem indicando que é um palíndromo.
+7. Caso contrário, exiba a palavra original e a palavra invertida para mostrar a diferença.
+
+Lembre-se de que é importante considerar a manipulação de strings e o uso dos métodos apropriados para comparar as palavras e construir a palavra invertida.
 
 */
 
