0ca912a1dd410d5eb8a9514bd7473cd7b632c557
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-31 22:46:27 -0300

    Aula sobre arrays

---
+++ modulo_1/aula-32/array.js
@@ -0,0 +1,8 @@
+const listaDeCompras = ["Açucar", false]
+console.log(listaDeCompras)
+
+    listaDeCompras [0] = "Arroz"
+    listaDeCompras [1] = "Feijão"
+    listaDeCompras [2] = 2
+
+    console.log(listaDeCompras)
