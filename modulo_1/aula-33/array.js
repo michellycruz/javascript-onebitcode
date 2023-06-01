0d13dbcd0d01ce2e8bea2cde5d5ffb0045e30c3f
Mich <125368412+michellycruz@users.noreply.github.com>
2023-05-31 23:34:17 -0300

    tipos de uso do array

--- modulo_1/aula-33/array.js
+++ modulo_1/aula-33/array.js
@@ -7,13 +7,30 @@ let tamanho = arr.push("Reaper")
 console.log(arr)
 console.log(tamanho)
 
-//unshift
-tamanho = arr.unshift("Reaper")
+//unshift 
+tamanho = arr.unshift("Teste")
 console.log(arr)
 console.log(tamanho)
 
 //Remover Elementos
 //pop
-const ultimoElemento = arr.pop()
+let ultimoElemento = arr.pop()
 console.log(arr)
 console.log(ultimoElemento)
+
+//Remover elementos no começo do array
+//shift
+ultimoElemento = arr.shift()
+console.log(arr)
+console.log(ultimoElemento)
+
+//Pesquisar por um elemento
+//includes
+const inclui = arr.includes("Torbjorn")
+console.log(inclui)
+
+//indexOf <-saber a posição no array
+const indice = arr.indexOf("Torbjorn")
+console.log(indice)
+
+//cortar e concatenar
