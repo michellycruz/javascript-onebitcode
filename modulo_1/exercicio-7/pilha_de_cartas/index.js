e2792f66777b7f922c98c75e604e4e5d2179783c
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-01 12:40:57 -0300

    exercício 7

---
+++ modulo_1/exercicio-7/pilha_de_cartas/index.js
@@ -0,0 +1,4 @@
+/*
+# [Escreva um programa em javascript para simular um baralho de cartas. O programa deve iniciar mostrando na tela um menu interativo contendo as cartas que estão atualmente no baralho e as opções de “Adicionar uma carta”, “Puxar uma carta” e “Sair”. Ao escolher “Adicionar uma carta”, o programa deve perguntar o nome da carta e adicioná-la no topo do baralho. Ao escolher “Puxar uma carta”, o programa deve retirar a carta do topo do baralho e mostrar na tela o nome da carta puxada. O programa só deve ser encerrado ao escolher a opção de “Sair”, caso contrário deve voltar ao menu.](https://www.mindmeister.com/map/2783164268)
+
+*/
