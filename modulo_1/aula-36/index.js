d8428056190ce79b2050e70c0649c29ff18bd4ad
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-01 23:21:25 -0300

    Update index.js

--- modulo_1/aula-36/index.js
+++ modulo_1/aula-36/index.js
@@ -1,11 +1,22 @@
 const arr = [
-    "1º Nível"
+    "1º Nível",
     ["2º Nível", 42, true],
     [
-        ["3º Nível, 1º item", "Olá mundo"]
-        ["3º Nível, 2º item", "oi mundo"]
+        ["3º Nível, 1º item", "Olá mundo"],
+        ["3º Nível, 2º item", "oi mundo"],
     ],
     []
 ]
 
+//acessando arrays bidimencionais 
+
 console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[1][1])
+
+//terceiro nivel
+console.log(arr[2])
+console.log(arr[2][1])
+console.log(arr[2][1][0])
