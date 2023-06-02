ffb217e943c0e59b059e00dd95461de00ada7467
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-01 23:24:59 -0300

    Update index.js

--- modulo_1/aula-36/index.js
+++ modulo_1/aula-36/index.js
@@ -20,3 +20,9 @@ console.log(arr[1][1])
 console.log(arr[2])
 console.log(arr[2][1])
 console.log(arr[2][1][0])
+
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
