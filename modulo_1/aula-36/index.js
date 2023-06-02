de19433aab51656db0ae49dd2e77b094ed79d16e
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-01 23:13:45 -0300

    Update index.js

---
+++ modulo_1/aula-36/index.js
@@ -0,0 +1,11 @@
+const arr = [
+    "1º Nível"
+    ["2º Nível", 42, true],
+    [
+        ["3º Nível, 1º item", "Olá mundo"]
+        ["3º Nível, 2º item", "oi mundo"]
+    ],
+    []
+]
+
+console.log(arr)
