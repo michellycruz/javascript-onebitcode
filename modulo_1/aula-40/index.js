e094dbe61f66f419845b21d6629fc2b3301cbb98
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 14:13:12 -0300

    aula 40 (OBJETOS)

---
+++ modulo_1/aula-40/index.js
@@ -0,0 +1,11 @@
+let pessoa = {}
+pessoa.idade = 26
+pessoa.nome = "Michelly"
+pessoa.colegas = ["Jennifer", "Gisele", "Vinicius"]
+pessoa.endereco = {
+    rua: "Pinho",
+    numero: 107,
+    bairro: "Colina de Laranjeiras"
+}
+
+console.log(pessoa)
