62780bc198073abe3f822cf1568733860e49a515
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 14:36:40 -0300

    Update index.js

--- modulo_1/exercicio-8/cadastro_de_imoveis/index.js
+++ modulo_1/exercicio-8/cadastro_de_imoveis/index.js
@@ -11,5 +11,34 @@
     - O menu também deve ter a opção de mostrar todos os imóveis salvos.](https://www.mindmeister.com/map/2783164284) */
 
     const imoveis=[]
-    const imovel=""
+    let opcao = ""
+
+    do{
+        opcao = prompt("Bem vindo ao cadastro de imóveis, o que deseja fazer?" + " Imóveis: " + imoveis.length + "\n\n1 - Salvar um imóvel" + "\n2 - Mostrar todos os imóveis salvos" + "\n3 - Sair")
+
+        switch(opcao){
+            case 1:{
+                imoveis.proprietario = prompt("Qual o nome do proprietário?")
+                imoveis.quartos = prompt("Qual a quantidade de quartos que o imóvel tem?")
+                imoveis.banheiros = prompt("Quantos banheiros?")
+                imoveis.garagem = prompt("Possui garagem?")
+            }
+
+            break
+
+            case 2: {
+                
+
+            }
+
+            break
+
+            case 3: {
+                alert("Encerrando...")
+            }
+            default:
+                alert("Comando inválido")
+        }
+        
+    }while(opcao !== 3)
 
