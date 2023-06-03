ef79feaa910a481567f0d16d0517d3d6c5f45de5
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 14:21:38 -0300

    exercicio cadastro de imoveis

---
+++ modulo_1/exercicio-8/cadastro_de_imoveis/index.js
@@ -0,0 +1,15 @@
+/* # [Escreva um programa em javascript que funcione como um cadastro de imóveis e atenda aos seguintes requisitos: 
+    
+    - Deve ter um menu interativo que sempre é exibido até que o usuário escolha sair. 
+    - O menu interativo deve mostrar no topo a quantidade de imóveis cadastrados. 
+    - O menu deve ter a opção de salvar um imóvel. 
+    - Para salvar um novo imóvel o programa deve pedir as seguintes informações:     
+    - Nome do proprietário.     
+    - Quantidade de quartos.     
+    - Quantidade de banheiros.     
+    - Se possui garagem. 
+    - O menu também deve ter a opção de mostrar todos os imóveis salvos.](https://www.mindmeister.com/map/2783164284) */
+
+    const imoveis=[]
+    const imovel=""
+
