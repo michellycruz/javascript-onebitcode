f0d2eb280bd1c6443369f9d78caf1af0fd68f2a6
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-03 23:26:03 -0300

    Update index.js

--- modulo_1/aula-43/index.js
+++ modulo_1/aula-43/index.js
@@ -39,3 +39,12 @@ function ola() {
 
 console.log(ola())
 
+function maiorIdade(idade){
+    if(idade >= 18){
+        return "Maior de idade"
+    }else {
+        return "Menor de idade"
+    }
+}
+
+    console.log(maiorIdade(20))
