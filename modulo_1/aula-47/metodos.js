f7af588fc0d2d56d33477b7649e3d8b473f448f3
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-05 00:18:02 -0300

    métodos

---
+++ modulo_1/aula-47/metodos.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome:"Michelly",
+    idade: 26,
+    dizerOla(){
+        console.log("Olá, mundo! Meu nome é " + this.nome)
+    }
+}
+
+//console.log(pessoa)
+
+pessoa.dizerOla()
