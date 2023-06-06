6eaeefe61fbe6478f383c55771e0ff188dce1409
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-06 00:35:52 -0300

    Update recursao.js

--- modulo_1/aula-48/recursao.js
+++ modulo_1/aula-48/recursao.js
@@ -19,4 +19,13 @@ function dobrar(num){
 // fatorial de 5: !5 === 5 * 4 * 3 * 2 * 1 === 5 * !4
 function fatorial(num){
     console.log("Número: " + num)
+    if (num === 0){ //caso base
+        return 1
+    } else if (num === 1) {
+        return 1
+    } else {
+        console.log(num + " * !" + (num - 1))
+        return num * fatorial(num - 1)
+    }
 }
+console.log(fatorial(5))
