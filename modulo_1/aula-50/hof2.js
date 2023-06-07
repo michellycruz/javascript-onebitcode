61ae1ca522367d1c669ffef61830837ebe47e4be
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-06 21:26:40 -0300

    Update hof2.js

--- modulo_1/aula-50/hof2.js
+++ modulo_1/aula-50/hof2.js
@@ -12,3 +12,14 @@ const lista = ["Maçã", "Banana", "Laranja", "Limão"]
 for (let i = 0; i< lista.length; i++){
     exibirElemento(lista[i], i, lista)
 }
+
+lista.forEach(exibirElemento) //callBackfn
+
+
+lista.forEach(function (elemento, indice, array){
+    console.log({
+        elemento,
+        indice,
+        array
+    })
+})
