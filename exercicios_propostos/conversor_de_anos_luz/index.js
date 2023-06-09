e16c00ce15ff77e95de2475a1329709e6a3d98c2
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-09 19:16:30 -0300

    conversor de anos luz

---
+++ exercicios_propostos/conversor_de_anos_luz/index.js
@@ -0,0 +1,24 @@
+let LightYear = prompt("Qual a distância em anos luz você gostaria de converter?")
+
+
+let opcao = prompt("Para qual medida de distância gostaria de converter?" + "\n1 - Parsec (PC)" + "\n2 - Unidade Astronômica (UA)" + "\n3 - Quilômetro (KM)" + "\n4 - Sair")
+
+switch(opcao){
+    case "1":
+        let parsec = (LightYear * 0.3066)
+        alert(LightYear + " LY" + " = " + parsec + " PC")
+        break
+    case "2":
+        let UnidadeAstronomica = LightYear * 632411
+        alert(LightYear + " LY" + " = " + UnidadeAstronomica + " UA")
+        break
+    case "3":
+        let quilometro = LightYear * (9.5 * Math.pow(10, 12))
+        alert(LightYear + " LY" + " = " + quilometro + " KM")
+        break
+    case "4":
+        alert("Encerrando...")
+        break
+    default: 
+        alert("Comando inválido")
+}
