7e3a0a82a21463273b16384c1ac157c31ecc5732
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-09 15:50:50 -0300

    Update index.js

--- exercicios_propostos/navegacao_nave/index.js
+++ exercicios_propostos/navegacao_nave/index.js
@@ -2,22 +2,27 @@ alert("Bem vindo ao sistema de navegação para astronautas!!")
 
 let velocidade = 0
 let piloto = prompt("Informe o nome do piloto")
-velocidade = prompt("A que velocidade você gostaria de acelerar a nave?")
+let novaVelocidade = prompt("A que velocidade você gostaria de acelerar a nave?")
+let confirmVelocidade = confirm("Estamos acelerando para " + novaVelocidade + "km/s")
+
+if(confirmVelocidade){
+    velocidade = novaVelocidade
+}
 
 alert("Você está a " + velocidade + "km")
 
-if(velocidade < 0){
+if(velocidade <= 0){
     alert("A nave está parada. Considere partir ou aumentar a velocidade")
     velocidade = prompt("A que velocidade você gostaria de acelerar a nave?")
 }else if(velocidade < 40){
     alert("Você está devagar, podemos aumentar mais")
     velocidade = prompt("A que velocidade você gostaria de acelerar a nave?")
-}else if(velocidade >= 40 && velocidade < 80){
+}else if(velocidade < 80){
     alert("Parece uma boa velocidade para manter")
-}else if(velocidade >= 80 && velocidade <= 100){
+}else if(velocidade < 100){
     alert("Velocidade alta, considere diminuir")
     velocidade = prompt("A que velocidade você gostaria de acelerar a nave?")
-}else if(velocidade > 100){
+}else {
     alert("Velocidade perigosa. Controle automático forçado")
 }
 
