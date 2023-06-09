76bb23a3d973f5e01612700390c43c00d8a1be37
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-09 11:08:59 -0300

    Update index.js

--- modulo_1/exercicio-10/index.js
+++ modulo_1/exercicio-10/index.js
@@ -1,18 +1,50 @@
 const vagas = []
 
+
 function listarVagas(){
-    
+    const vagasEmTexto = vagas.reduce(function(textoFinal, vaga, indice){
+        textoFinal += indice + ". "
+        textoFinal += vaga.nome
+        textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
+        return textoFinal
+    }, "")
+    alert(vagasEmTexto)
 }
 
-function executarMenu (){
-    return prompt("Sistema de vagas de emprego -" + " Selecione uma opção" +
-    "\n1 - Listar vagas disponíveis" +
-    "\n2 - Criar uma nova vaga" +
-    "\n3 - Visualizar uma vaga" +
-    "\n4 - Inscrição" +
-    "\n5 - Excluir vaga" +
-    "\n6 - Sair"
+function novaVaga(){
+    const nome = prompt("Informe o nome da vaga:")
+    const descricao = prompt("Informe a descricao da vaga:")
+    const dataLimite = prompt("Informe a data limite da vaga / (dd/mm/aaaa):")
+
+    const confirmacao = confirm(
+        "Deseja salvar essa vaga com as seguintes informações?" +
+        "\nNome: " + nome +
+        "\nDescrição: " + descricao +
+        "\nData limite: " + dataLimite
+    )
+
+    if (confirmacao){
+        const novaVaga = { nome, descricao, dataLimite, candidatos: [] }
+        vagas.push(novaVaga)
+        alert("Vaga criada.")
+    }
+}
+
+function exibirVaga(){
+    const indice = prompt("Informe o indice da vaga que deseja exibir: ")
+    const vaga = vagas[indice]
+
+    const candidatosEmTexto = vagas.candidatos.reduce(function(textoFinal, candidato){
+        return textoFinal + "\n - " + candidato
+    }, "")
+
+    alert(
+        "Vaga nº " + indice +
+        "\nNome: " + vaga.nome +
+        "\nDescrição: " + vaga.descricao +
+        "\nData limite: " + vaga.dataLimite +
+        "\nQuantidade de candidatos: " + vaga.candidatos.length +
+        "\nCandidatos inscritos: " + candidatosEmTexto
     )
 }
-
 
