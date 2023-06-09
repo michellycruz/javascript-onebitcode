19363c0d7603815b79e12e3dd64e75cc98c80355
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-08 22:33:12 -0300

    Update index.js

---
+++ modulo_1/exercicio-10/index.js
@@ -0,0 +1,11 @@
+const executarMenu = function (){
+    return prompt("Sistema de vagas de emprego -" + " Selecione uma opção" +
+    "\n1 - Listar vagas disponíveis" +
+    "\n2 - Criar uma nova vaga" +
+    "\n3 - Visualizar uma vaga" +
+    "\n4 - Inscrição" +
+    "\n5 - Excluir vaga" +
+    "\n6 - Sair"
+    )
+}
+
