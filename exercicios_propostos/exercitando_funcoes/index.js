57f360996fa181cd1aed81d7bad2e64a275d7086
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-10 16:21:24 -0300

    Update index.js

--- exercicios_propostos/exercitando_funcoes/index.js
+++ exercicios_propostos/exercitando_funcoes/index.js
@@ -6,14 +6,12 @@ let opcao = ""
 function acelerar(){
     velocidadeNave += 5
 }
-
 function desacelerar(){
     velocidadeNave -= 5
 }
 
 do{
     switch(opcao){
-
         case "1":
             acelerar()
             alert("Acelerando... ")
@@ -29,8 +27,10 @@ do{
             alert("Encerrando o programa")
         break
     }  
-    opcao = prompt("O que deseja fazer?" + "\n1 - Acelerar a nave" + "\n2 - Desacelerar a nave" + "\n3 - Sair")
+    opcao = prompt("O que deseja fazer?" + 
+                   "\n1 - Acelerar a nave" + 
+                   "\n2 - Desacelerar a nave" + 
+                   "\n3 - Sair")
 } while(opcao != "3")
 
-
-alert("| Dados de bordo |" + "\nNome da nave: " + nave + "\nVelocidade: " + velocidadeNave + "km")
+alert("| Dados de bordo |" + "\nNome da nave: " + nave + "\nVelocidade: " + velocidadeNave + "km/s")
