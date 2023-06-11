61152d06e2d99a6f186c5943c5c0c04b01041fa7
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-11 18:12:42 -0300

    eventos

---
+++ modulo_2/eventos_aula01/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter(){
+    console.log('Entrada do mouse')
+}
+
+function onChange(){
+    console.log('Mudança no valor')
+}
+
+function onFocus(){
+    console.log('foco no elemento')
+}
+
+function onBlur(){
+    console.log('desfocado o elemento')
+}
+
+function onClick(){
+    console.log('clicou no botão')
+}
