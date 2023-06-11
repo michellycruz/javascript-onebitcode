732a24c3a1e2283304f06447fc2b3cd770984084
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-11 12:50:26 -0300

    Update index.js

--- exercicios_propostos/praticando_arrays_ex10/index.js
+++ exercicios_propostos/praticando_arrays_ex10/index.js
@@ -6,16 +6,23 @@ const hitchedSpaceships = [
     ["Darwin", 15, false]
 ];
 
-function maisde9Tripulantes(nave){
-    const tripulantes = nave[1]
-    if(tripulantes >= 9){
-        return true
-    } else {
-        return false
-    }
-};
+let maisde9Tripulantes = hitchedSpaceships.filter(nave => {
+    return nave[1] > 9
+}).map(nave => {
+    return spaceship[0]
+})
+
+let engatePendente = hitchedSpaceships.findIndex(nave =>{
+    return nave[2] == false
+})
+
+let naveEngatada = hitchedSpaceships.map(nave => {
+    return nave[0].toUpperCase()
+})
 
 
-const naveFiltrada = hitchedSpaceships.filter((nave) => maisde9Tripulantes(nave));
+let message = "Espaçonaves com mais de 9 tripulantes: " + maisde9Tripulantes.join(", ")
+message += "\nPlataforma com processo de engate: " + (engatePendente + 1)
+message += "\nEspaçonaves destacadas: " + naveEngatada.join(", ")
 
-console.table(naveFiltrada)
+alert(message)
