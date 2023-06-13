f5506b7d6c22122895ea77de77c412054fb42e63
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-13 10:58:47 -0300

    Update index.js

--- modulo_1/exercicio-1/cadastro_de_recruta/index.js
+++ modulo_1/exercicio-1/cadastro_de_recruta/index.js
@@ -1,13 +1,16 @@
 /*
 # [Escreva um programa em javascript que permita salvar informações de um recruta. As informações a serem salvas são: - o primeiro nome - o sobrenome - o campo de estudo - o ano de nascimento Depois o programa deve exibir o nome completo do recruta, seu campo de estudo e sua idade (apenas baseada no ano de nascimento).](https://www.mindmeister.com/map/2783164111)
  */
-
 const nome = prompt("Informe o seu nome: ")
-const sobreNome = prompt("Informe seu sobre nome: ")
+const sobreNome = prompt("Informe seu sobrenome: ")
 const campoDeEstudo = prompt("Informe o campo de estudo: ")
 const nascimento = prompt("Informe seu ano de nascimento: ")
 
 let anoAtual = new Date().getFullYear()
-let idade = 2023 - 1998
 
-console.log(idade)
+let idade = anoAtual - nascimento
+
+alert("| Informações do recruta |" + 
+      "\n\nNome completo: " + nome + " " + sobreNome +
+      "\nCampo de estudo: " + campoDeEstudo +
+      "\nIdade: " + idade)
