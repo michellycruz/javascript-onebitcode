79e68ffd8c022f79424e3dbdc5a551dac118c00a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-16 20:23:39 -0300

    Update index.js

--- modulo_1/exercicio-4/index.js
+++ modulo_1/exercicio-4/index.js
@@ -2,23 +2,19 @@
 # [Escreva um programa em javascript que peça o nome de um turista e então pergunte se ele já visitou alguma cidade. Caso a resposta seja sim, pergunte o nome da cidade e o armazene em uma variável, e então continue perguntando se o turista visitou alguma outra cidade até que a resposta seja não. No fim, o programa deve exibir o nome do turista, quantas e quais cidades ele visitou.](https://www.mindmeister.com/map/2783164177)
 */
 
-
-const turista = prompt("Eai turista!! qual o seu nome?")
-let cidades = ""
+const turista = prompt("Eai turista?! Qual o seu nome??")
+let visitou = prompt("Você já visitou alguma cidade?")
+let cidade = ""
 let contagem = 0
-let continuar = prompt("Você já visitou alguma cidade? sim/não")
+let cidadesVisitadas = ""
 
-    while(continuar === "sim"){
-        let cidade = prompt("Qual o nome da cidade que você visitou?")
-        cidades += " - " + cidade + "\n"
-        contagem++
-        continuar = prompt("Você já visitou alguma outra cidade? sim/não")
-    }
+while(visitou === "sim"){
+    cidade = prompt("Qual o nome da cidade você visitou?")
+    cidadesVisitadas += "\n- " + cidade
+    contagem++
+    visitou = prompt("Você visitou mais alguma?")
+}
 
-
-alert(
-    "Nome do turista: " + turista + "\n" +
-    "Quantidade de cidades visitadas: " + contagem + "\n" +
-    "As cidades foram:\n" + cidades
-)
-
+alert("Nome do turista: " + turista +
+      "\nCidades visitadas: " + contagem +
+      "\nNomes das cidades: " + cidadesVisitadas)
