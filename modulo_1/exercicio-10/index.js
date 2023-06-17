3875b88e3116fd83e3720120320d322875edeeb4
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-17 14:15:21 -0300

    Update index.js

--- modulo_1/exercicio-10/index.js
+++ modulo_1/exercicio-10/index.js
@@ -8,7 +8,13 @@ function listarVagas(){
         textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
         return textoFinal
     }, "")
-    alert(vagasEmTexto)
+
+    if(vagasEmTexto){
+        alert(vagasEmTexto)
+    } else {
+        alert("Não há vagas disponíveis")
+    }
+
 }
 
 function novaVaga(){
@@ -115,27 +121,21 @@ function executar(){
             case "1":
                 listarVagas()
                 break 
-
             case "2":
                 novaVaga()
                 break
-
             case "3":
                 exibirVaga()
                 break
-
             case "4": 
                 inscreverCandidato()
                 break
-
             case "5": 
                 excluirVaga()
                 break
-
             case "6":
                 alert("Finalizando o programa...") 
                 break
-
             default:
                 alert("Comando inválido")
         }
