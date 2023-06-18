4931fce1b54c0f8c22f7747ef943927913689e38
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-18 17:46:54 -0300

    remove listener

--- modulo_2/11_trabalhando_com_eventos/index.js
+++ modulo_2/11_trabalhando_com_eventos/index.js
@@ -1,5 +1,22 @@
+function register(ev){
+    console.log(ev)
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+
+    if(password === passwordConfirmation){
+        alert('Usuário ' + username + ' registrado!')
+    } else{
+        alert('As senhas não conferem!')
+    }
+}
+
 const button = document.getElementById('register-button')
 
-button.addEventListener('click', function(){
-    alert('Botão clicado')
-})
+button.addEventListener('click', register)
+
+function removeListener(){
+    button.removeEventListener('click', register)
+    alert('Event removed')
+}
