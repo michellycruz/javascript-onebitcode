4e38964bf2e4a4778e2451938da931d67a6bb1e8
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-19 20:56:49 -0300

    exercicio cadastro de devs

---
+++ modulo_2/13_exercicio_12/index.js
@@ -0,0 +1,10 @@
+/*
+Construa uma página web que permita cadastrar desenvolvedores, suas tecnologias e tempos de experiência. 
+- A página deve ter um input para o nome completo do desenvolvedor e um botão para adicionar uma nova tecnologia. - Ao clicar no botão um evento deve ser disparado criando no formulário uma nova linha de inputs contendo:     
+- um campo de texto para o nome da tecnologia     
+- um grupo de botões tipo “radio” com opções de tempo de experiência (por exemplo, “0-2 anos”, “3-4 anos” e “5+ anos”)     
+- um botão de remover que deve excluir essa linha.     
+- O formulário também deve ter um botão de cadastrar que salva as informações do dev em um array e limpa o formulário. Para testar o funcionamento você pode utilizar um simples console.log() 
+
+Para esse exercício você deve utilizar apenas eventos adicionados via javascript e a página deve funcionar sem acionar um recarregamento.
+*/
