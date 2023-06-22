547992f61e451eee8ff7d3bd822a37d19a6724b2
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-22 20:16:31 -0300

    Update index.js

--- modulo_2/13_exercicio_12/index.js
+++ modulo_2/13_exercicio_12/index.js
@@ -44,10 +44,25 @@ addTechBtn.addEventListener('click', function(ev){
     const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
 
     const expLabel = createLabel('Experiência: ')
-    const expRadio1 = 
+    const id1 = 'expRadio-' + rowIndex + '.1'
+    const expRadio1 = createInput(id1, '0-2 anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel1 = createLabel('0-2 anos', id1)
+    const id2 = 'expRadio-' + rowIndex + '.2'
+    const expRadio2 = createInput(id2, '3-4 anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel2 = createLabel('3-4 anos', id2)
+    const id3 = 'expRadio-' + rowIndex + '.3'
+    const expRadio3 = createInput(id3, '5+ anos', 'techExp-' + rowIndex, 'radio')
+    const expLabel3 = createLabel('5+ anos', id3)
+
+    const removeRowBtn = document.createElement('button')
+    removeRowBtn.type = 'button'
+    removeRowBtn.innerText = 'Remover'
+    removeRowBtn.addEventListener('click', function(){
+        stackInputs.removeChild(newRow)
+    })
 
     newRow.append(
-        techNameLabel, techNameInput
+        techNameLabel, techNameInput, expLabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3, removeRowBtn
     )
 
     stackInputs.appendChild(newRow)
