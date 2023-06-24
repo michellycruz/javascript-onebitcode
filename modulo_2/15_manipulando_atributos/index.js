e30dc5f6da476754f1f9147addcfca54c16d7ff8
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-23 22:23:33 -0300

    getAtribute

--- modulo_2/15_manipulando_atributos/index.js
+++ modulo_2/15_manipulando_atributos/index.js
@@ -1,7 +1,7 @@
 const input = document.getElementById('input')
 
 document.getElementById('value').addEventListener('click', function(){
-    input.value = input.value === '' ? 'Olá, Mundo!' : ''
+    input.value = input.value = 'Olá, Mundo!'
 
     console.log(input.value)
     console.log(input.getAttribute('value'))
