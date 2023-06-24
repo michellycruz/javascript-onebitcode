574b72e8606ee30f11eb66aab7d3a3723c4ef3ce
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-23 22:20:11 -0300

    Update index.js

--- modulo_2/15_manipulando_atributos/index.js
+++ modulo_2/15_manipulando_atributos/index.js
@@ -1,5 +1,8 @@
 const input = document.getElementById('input')
 
 document.getElementById('value').addEventListener('click', function(){
-    
+    input.value = input.value === '' ? 'Olá, Mundo!' : ''
+
+    console.log(input.value)
+    console.log(input.getAttribute('value'))
 })
