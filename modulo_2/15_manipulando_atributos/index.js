b04743ed9ee67423957b596dc0a0185a8c3ea1cb
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-23 22:28:26 -0300

    Update index.js

--- modulo_2/15_manipulando_atributos/index.js
+++ modulo_2/15_manipulando_atributos/index.js
@@ -6,3 +6,9 @@ document.getElementById('value').addEventListener('click', function(){
     console.log(input.value)
     console.log(input.getAttribute('value'))
 })
+
+document.getElementById('type').addEventListener('click', function(){
+   // input.type = input.type !== 'date' ? 'date' : 'text'
+    // ? > if   : > else
+    input.setAttribute('type', 'radio')
+})
