b1d411fa10a5f7c7742f12314b783cf8fb4ac583
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 00:49:03 -0300

    Update index.js

--- projetos/index.js
+++ projetos/index.js
@@ -2,3 +2,6 @@ const main = document.querySelector('main')
 const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const result = document.getElementById('result')
+
+const alloedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
