839fca8e68faf66c45f31163f5df190c8c81ed86
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 12:44:00 -0300

    Update index.js

--- projetos/index.js
+++ projetos/index.js
@@ -1,7 +1,7 @@
 const main = document.querySelector('main')
 const root = document.querySelector(':root')
 const input = document.getElementById('input')
-const result = document.getElementById('result')
+const resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
