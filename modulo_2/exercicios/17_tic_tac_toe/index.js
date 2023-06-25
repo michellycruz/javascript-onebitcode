300952e51629537b8cee56c578ff3a337195a661
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 15:33:50 -0300

    tic tac toe

--- modulo_2/exercicios/17_tic_tac_toe/index.js
+++ modulo_2/exercicios/17_tic_tac_toe/index.js
@@ -5,20 +5,32 @@ function gameSection(){
 
 gameSection()
 
-function form(){
-    const player1 = getElementById('player1').value 
-    const player2 = getElementById('player2').value 
-    console.log(player1, player2)
-}
-
 document.getElementById('startButton').addEventListener('click', function(ev){
     ev.preventDefault();
     start();
 })
 
 function start(){
+    const player1 = document.getElementById('player1').value 
+    const player2 = document.getElementById('player2').value 
+
+    if (player1.trim() === '' || player2.trim() === ''){
+        alert('Por favor, insira os nomes dos jogadores.')
+        return
+    }
+
     const formSection = document.getElementById('formSection')
     formSection.style.display = 'none'
     const gameSection = document.getElementById('gameSection')
     gameSection.style.display = 'block'
+}
+
+function addPlayers(){
+    const ul = document.getElementById('players')
+
+    const newLi = document.createElement('li')
+    newLi.className = 'list-item'
+    newLi.innerText = player1 + ' versus ' + player2
+
+    ul.appendChild(newLi)
 }
