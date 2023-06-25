5d526efb35e6017c0699d9c971f917e72e700269
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 00:55:22 -0300

    Update index.js

--- projetos/index.js
+++ projetos/index.js
@@ -5,3 +5,10 @@ const result = document.getElementById('result')
 
 const alloedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+input.addEventListener('keydown', function (ev){
+    ev.preventDefault()
+    if (alloedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }
+})
