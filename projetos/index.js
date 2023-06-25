30aaeae8c655a81d6c7e2b221223911e626cfc8d
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 00:47:07 -0300

    Update index.js

---
+++ projetos/index.js
@@ -0,0 +1,4 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const result = document.getElementById('result')
