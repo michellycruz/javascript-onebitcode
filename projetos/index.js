ae2e086e4c0667131f076292a302035ace6969d7
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-25 13:14:37 -0300

    Update index.js

--- projetos/index.js
+++ projetos/index.js
@@ -38,6 +38,15 @@ function calculate(){
     resultInput.value = result
 }
 
+document.getElementById('copyToClipboard').addEventListener('click', function(ev){
+    const button = ev.currentTarget
+    if(button.innerText === 'Copy'){
+        button.innerText = 'Copied!'
+        button.classList.add('success')
+        
+    }
+})
+
 document.getElementById('themeSwitcher').addEventListener('click', function(){
     if(main.dataset.theme === 'dark'){
         root.style.setProperty('--bg-color', '#f1f5f9')
@@ -46,10 +55,10 @@ document.getElementById('themeSwitcher').addEventListener('click', function(){
         root.style.setProperty('--primary-color', '#26834a')
         main.dataset.theme = 'light'
     } else {
-        root.style.setProperty('--bg-color', '#f1f5f9')
-        root.style.setProperty('--border-color', '#aaa')
-        root.style.setProperty('--font-color', '#212529')
-        root.style.setProperty('--primary-color', '#26834a')
-        main.dataset.theme = 'light'
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91');
+        main.dataset.theme = 'dark'
     }
 })
