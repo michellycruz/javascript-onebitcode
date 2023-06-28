14aa9eace6b4221cbd1c0ab30f31a3e4030c60be
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-27 21:09:53 -0300

    exercicio jogo da velha

--- modulo_2/exercicios/17_tic_tac_toe/index.js
+++ modulo_2/exercicios/17_tic_tac_toe/index.js
@@ -1,41 +1,28 @@
-// Variáveis globais
-let turnPlayer = '';
-let btnRef;
+const boardRegions = document.querySelectorAll('#gameBoard span')
+let vBoard = []
+let turnPlayer = ''
 let restartBtn = document.getElementById('resetButton');
-let winningPattern = [
-  [0, 1, 2],
-  [0, 3, 6],
-  [2, 5, 8],
-  [6, 7, 8],
-  [3, 4, 5],
-  [1, 4, 7],
-  [0, 4, 8],
-  [2, 4, 6]
-];
-let xTurn = true;
-let count = 0;
 
 // Função para atualizar o título com o nome do jogador
 function updateTitle() {
   const playerInput = document.getElementById(turnPlayer);
-  document.getElementById('turnPlayer').innerText = playerInput.value;
+  document.getElementById('turnPlayer').innerText = playerInput.value
 }
 
 // Oculta a seção de jogo
 function hideGameSection() {
-  const gameSection = document.getElementById('gameSection');
-  gameSection.style.display = 'none';
+  const gameSection = document.getElementById('gameSection')
+  gameSection.style.display = 'none'
 }
 
 hideGameSection();
 
-// Event listener para o botão de começar o jogo
 document.getElementById('startButton').addEventListener('click', function(ev) {
-  ev.preventDefault();
-  start();
+  ev.preventDefault()
+  start()
 });
 
-// Função de inicialização do jogo
+// botão "começar" após do preenchimento dos nomes
 function start() {
   const player1 = document.getElementById('player1').value;
   const player2 = document.getElementById('player2').value;
@@ -49,69 +36,25 @@ function start() {
   formSection.style.display = 'none';
   const gameSection = document.getElementById('gameSection');
   gameSection.style.display = 'block';
-
-  addPlayers(player1, player2);
-
-  btnRef = document.querySelectorAll('.button-option');
-  btnRef.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      if (xTurn) {
-        element.innerText = 'X';
-        element.disabled = true;
-        xTurn = false;
-      } else {
-        element.innerText = 'O';
-        element.disabled = true;
-        xTurn = true;
-      }
-      count += 1;
-      if (count === 9) {
-        drawFunction();
-      }
-      winChecker();
-    });
-    element.classList.add('button-option'); // Adiciona a classe '.button-option' aos botões
-  });
-
-  enableButtons();
-}
-
-// Função para desabilitar os botões
-const disableButtons = () => {
-  btnRef.forEach((element) => {
-    element.disabled = true;
-  });
-};
-
-// Função para habilitar os botões
-const enableButtons = () => {
-  btnRef.forEach((element) => {
-    element.innerText = '';
-    element.disabled = false;
-  });
-};
-
-// Event listener para o botão de recomeçar o jogo
-restartBtn.addEventListener('click', () => {
-  count = 0;
-  enableButtons();
-});
-
-// Função para verificar se há um vencedor
-function winChecker() {
-  for (let i = 0; i < winningPattern.length; i++) {
-    const [a, b, c] = winningPattern[i];
-    if (
-      btnRef[a].innerText !== '' &&
-      btnRef[a].innerText === btnRef[b].innerText &&
-      btnRef[a].innerText === btnRef[c].innerText
-    ) {
-      const winner = xTurn ? document.getElementById('player1').value : document.getElementById('player2').value;
-      disableButtons();
-      alert(`O jogador ${winner} venceu!`);
-      return;
-    }
   }
-}
 
-// Função para
+  function initializeGame(){
+    vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+    turnPlayer = 'player1'
+    document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+    updateTitle()
+    boardRegions.forEach(function(element){
+      element.classList.remove('win')
+      element.innerText = ''
+      element.addEventListener('click', handleBoardClick)
+    })
+  }
+
+  function handleBoardClick(ev){
+    const region = ev.currentTarget.dataset.region
+    const rowColumnPair = region.slit('.')
+    const row = rowColumnPair[0]
+    const column = rowColumnPair[1]
+  }
+
+document.getElementById('startButton').addEventListener('click', initializeGame)
