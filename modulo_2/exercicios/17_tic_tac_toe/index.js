502a968dcf2c7966f266d276e345af4b5528b3da
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-27 21:44:38 -0300

    Update index.js

--- modulo_2/exercicios/17_tic_tac_toe/index.js
+++ modulo_2/exercicios/17_tic_tac_toe/index.js
@@ -50,6 +50,27 @@ function start() {
     })
   }
 
+  function getWinRegions(){
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+      winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+      winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+      winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+      winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+      winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+      winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+      winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+  }
+
   function disableRegion(element){
     element.style.cursor = 'default'
     element.removeEventListener('click', handleBoardClick)
@@ -71,6 +92,7 @@ function start() {
     console.clear()
     console.table(vBoard)
     disableRegion(span)
+    const winRegions = getWinRegions
   }
 
 document.getElementById('startButton').addEventListener('click', initializeGame)
