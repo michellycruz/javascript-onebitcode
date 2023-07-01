78c3460b9f907f0632e8ef6e48ed7bc888378170
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-01 16:47:30 -0300

    Update index.js

--- modulo_3/arrow_functions/index.js
+++ modulo_3/arrow_functions/index.js
@@ -3,3 +3,16 @@ function normalSum (a, b){
 }
 
 console.log(`Soma normal: ${normalSum(3, 5)}`)
+
+
+const anonymousSum = function(a, b){
+    return a + b
+}
+
+console.log(`Soma anônima: ${anonymousSum(2, 7)}`)
+
+const arrowSum = (a, b) => {
+    return a + b
+}
+
+console.log(`Soma arrow function: ${arrowSum(3, 3)}`)
