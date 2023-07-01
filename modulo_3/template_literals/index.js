bf6f4d58f57a73bd1fe9cfde5510d04bef0b616d
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-01 10:45:40 -0300

    template literals

---
+++ modulo_3/template_literals/index.js
@@ -0,0 +1,19 @@
+
+
+const nome = 'João';
+const idade = 25;
+
+const mensagem = `Olá, meu nome é ${nome} e tenho ${idade} anos.`;
+
+console.log(mensagem);
+
+
+//mensagem multilinha
+
+const mensagemMultiLinha = `
+  Olá,
+  Bem-vindo ao nosso site!
+  Esperamos que você goste da sua estadia.
+`;
+
+console.log(mensagemMultiLinha);
