412aaaa709f0b4bece037780f2df2ad17f3a90c9
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-01 18:13:41 -0300

    Update index.js

--- modulo_3/desestruturacao_arrays_objetos/index.js
+++ modulo_3/desestruturacao_arrays_objetos/index.js
@@ -12,5 +12,15 @@ const [father, mother] = parents
 console.log(father, mother)
 
 function createUser(person){
-    
+    const id = Math.floor(Math.random() * 9999)
+    return {
+        id,
+        name: person.name,
+        job: person.job,
+        parents: person.parents
+    }
 }
+
+const luke = createUser(person)
+
+console.log(luke)
