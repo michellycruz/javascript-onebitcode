7d01a49d92e71b9a712eed5be9908766b703b493
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-01 17:03:44 -0300

    Update index.js

--- modulo_3/arrow_functions/index.js
+++ modulo_3/arrow_functions/index.js
@@ -1,28 +1,27 @@
 function normalSum (a, b){
     return a + b
 }
-
 console.log(`Soma normal: ${normalSum(3, 5)}`)
-
 
 const anonymousSum = function(a, b){
     return a + b
 }
-
 console.log(`Soma anônima: ${anonymousSum(2, 7)}`)
 
 const arrowSum = (a, b) => {
     return a + b
 }
-
 console.log(`Soma arrow function: ${arrowSum(3, 3)}`)
 
 const subtract = (a, b) => a - b
-
 console.log(`Subtração arrow function: ${subtract(10, 5)}`)
 
 const double = n => `O dobro de ${n} é ${n * 2}`
-
 const number = 32
-
 console.log(double(number))
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+const startWithP = towns.filter(towns => towns[0] === 'P')
+
+console.log(startWithP)
