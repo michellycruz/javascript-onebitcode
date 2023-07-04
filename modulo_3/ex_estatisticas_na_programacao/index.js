bc8e22270d9ef094da5438df55b97b181affccba
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-03 22:57:02 -0300

    Update index.js

--- modulo_3/ex_estatisticas_na_programacao/index.js
+++ modulo_3/ex_estatisticas_na_programacao/index.js
@@ -45,4 +45,8 @@ const mode = (...numbers) => {
         num,
         numbers.filter(n => num === n).length
     ])
+    quantities.sort((a, b) => b[1] - a[1])
+    return quantities[0][0]
 }
+
+console.log(`Moda: ${mode(1, 1, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4)}`)
