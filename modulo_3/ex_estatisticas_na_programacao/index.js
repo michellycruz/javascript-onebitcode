b3dfb004b0a5cb278a91dc70349dacd1039f1fbc
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-03 22:19:42 -0300

    Update index.js

--- modulo_3/ex_estatisticas_na_programacao/index.js
+++ modulo_3/ex_estatisticas_na_programacao/index.js
@@ -1,8 +1,11 @@
 ///////////MÉDIA ARITMÉTICA SIMPLES
 
-let media = (a, b, c, d, e) => {
-    return media.sum()
+let media = (...numbers) => {
+    const sum = numbers.reduce((accum, num) => accum + num, 0)
+    return sum / numbers.length
 }
 
 
-console.log(media)
+console.log(`Média aritmética simples: ${5, 4, 6, 8}`)
+
+//////////MÉDIA PONDERADA
