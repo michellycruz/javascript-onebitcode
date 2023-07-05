362408e08b599665000a9fce739aa33777f931e4
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-05 16:24:57 -0300

    desafios leetcode

---
+++ desafios_leetcode/hello_world/index.js
@@ -0,0 +1,9 @@
+let createHelloWorld = function(){
+    return() => 'Hello World' 
+}
+
+window.onload = function(){
+    let createdOutput = document.getElementById('output')
+    let helloWorld = createHelloWorld()
+    createdOutput.innerHTML = helloWorld()
+}
