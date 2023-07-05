c06ed4cc163aec0ce9e2273491d844a62eb74c42
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-04 22:35:52 -0300

    export default

--- modulo_3/tipos_de_export_esm/index.js
+++ modulo_3/tipos_de_export_esm/index.js
@@ -1,6 +1,7 @@
 import { inline } from "./inline.js"   //"inline" é um nome único para mudar o nome deve-se usar por ex "nome as inline. O "Export nomeado" pode ser exportado quantas vezes quiser
 import defaultInline from "./inline.js" //o nome é flexivel, "defaultInline" pode ser alterado. "Export Default" só pode ter 1 por arquivo
-import { group } from "./non_inline.js"
+import exportDefault, { group, a, b, c, d } from "./non_inline.js" // consigo informar exportações na mesma linha
+
 
 inline()
 defaultInline()
--- modulo_3/tipos_de_export_esm/non_inline.js
+++ modulo_3/tipos_de_export_esm/non_inline.js
@@ -2,8 +2,15 @@ function group(){
     console.log('Export nomeado não-inline (agrupado)')
 }
 
+function a () {}
+function b () {}
+function c () {}
+function d () {}
+
 function exportDefault(){
     console.log('Export default não-inline')
 }
 
-export {group}
+export {group, a, b, c, d}
+
+export default exportDefault
