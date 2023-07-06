4ef7114bef5b73c8c342e74973573b6e9a3d4012
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-06 14:26:39 -0300

    exercicio esmodules

---
+++ modulo_3/ex_ESmodules_calculadora/js/index.js
@@ -0,0 +1,72 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
+    charKeyBtn.addEventListener('click', function(){
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function(){
+    input.value = ''
+    input.focus()
+})
+
+input.addEventListener('keydown', function (ev){
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)){
+        input.value += ev.key
+        return
+    }
+    if (ev.key === 'Backspace'){
+        input.value = input.value.slice(0, -1)
+    }
+    if(ev.key === 'Enter'){
+        calculate()
+    }
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+function calculate(){
+    resultInput.value = 'ERROR'
+    resultInput.classList.add('error')
+    
+    const result = eval(input.value) //eval = avaliar //cuidado ao usar o eval
+    
+    resultInput.value = result
+    resultInput.classList.remove('error')
+}
+
+document.getElementById('copyToClipboard').addEventListener('click', function(ev){
+    const button = ev.currentTarget
+    if(button.innerText === 'Copy'){
+        button.innerText = 'Copied!'
+        button.classList.add('success')
+        navigator.clipboard.writeText(resultInput.value)  //copiar para a área de transferência
+    } else{
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+document.getElementById('themeSwitcher').addEventListener('click', function(){
+    if(main.dataset.theme === 'dark'){
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff91');
+        main.dataset.theme = 'dark'
+    }
+})
