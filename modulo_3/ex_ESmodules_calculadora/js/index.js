23a36fd60d0580156cb6f3f6f020cfda99a1b516
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-06 23:09:26 -0300

    theme switcher

--- modulo_3/ex_ESmodules_calculadora/js/index.js
+++ modulo_3/ex_ESmodules_calculadora/js/index.js
@@ -1,7 +1,6 @@
 import calculate from "./calculate.js"
+import copyToClipboard from "./copyToClipboard"
 
-const main = document.querySelector('main')
-const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
 
@@ -34,31 +33,6 @@ input.addEventListener('keydown', function (ev){
 })
 
 document.getElementById('equal').addEventListener('click', calculate)
+document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
 
-document.getElementById('copyToClipboard').addEventListener('click', function(ev){
-    const button = ev.currentTarget
-    if(button.innerText === 'Copy'){
-        button.innerText = 'Copied!'
-        button.classList.add('success')
-        navigator.clipboard.writeText(resultInput.value)  //copiar para a área de transferência
-    } else{
-        button.innerText = 'Copy'
-        button.classList.remove('success')
-    }
-})
-
-document.getElementById('themeSwitcher').addEventListener('click', function(){
-    if(main.dataset.theme === 'dark'){
-        root.style.setProperty('--bg-color', '#f1f5f9')
-        root.style.setProperty('--border-color', '#aaa')
-        root.style.setProperty('--font-color', '#212529')
-        root.style.setProperty('--primary-color', '#26834a')
-        main.dataset.theme = 'light'
-    } else {
-        root.style.setProperty('--bg-color', '#212529')
-        root.style.setProperty('--border-color', '#666')
-        root.style.setProperty('--font-color', '#f1f5f9')
-        root.style.setProperty('--primary-color', '#4dff91');
-        main.dataset.theme = 'dark'
-    }
-})
+document.getElementById('themeSwitcher').addEventListener('click', )
