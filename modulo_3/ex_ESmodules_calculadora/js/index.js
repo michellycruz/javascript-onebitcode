76d59063ca9ab70cdd2757c17234b68f552f2563
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-07 20:35:44 -0300

    correções

--- modulo_3/ex_ESmodules_calculadora/js/index.js
+++ modulo_3/ex_ESmodules_calculadora/js/index.js
@@ -1,14 +1,14 @@
 import calculate from "./calculate.js"
 import copyToClipboard from "./copyToClipboard"
-import { hadleClearButton, handleButtonPress, handleTyping } from "./keyHandles.js"
-import themeSwitcher from "./themeSwitcher.js"
+import { handleButtonPress, handleClear, handleTyping } from "./keyHandlers.js"
+import switchTheme from "./switchTheme.js"
 
 document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
     charKeyBtn.addEventListener('click', handleButtonPress)
 })
 
-document.getElementById('clear').addEventListener('click', hadleClearButton)
-document.getElementById('input').addEventListener('keydown', handleTyping)
-document.getElementById('equal').addEventListener('click', calculate)
-document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
-document.getElementById('themeSwitcher').addEventListener('click', themeSwitcher)
+document.getElementById('#clear').addEventListener('click', handleClear)
+document.getElementById('#input').addEventListener('keydown', handleTyping)
+document.getElementById('#equal').addEventListener('click', calculate)
+document.getElementById('#copyToClipboard').addEventListener('click', copyToClipboard)
+document.getElementById('#themeSwitcher').addEventListener('click', switchTheme)
