be7052a1b581acb572a72fc6ba754dd288e0dd76
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-07 20:41:55 -0300

    Update index.js

--- modulo_3/ex_ESmodules_calculadora/js/index.js
+++ modulo_3/ex_ESmodules_calculadora/js/index.js
@@ -7,8 +7,8 @@ document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
     charKeyBtn.addEventListener('click', handleButtonPress)
 })
 
-document.getElementById('#clear').addEventListener('click', handleClear)
-document.getElementById('#input').addEventListener('keydown', handleTyping)
-document.getElementById('#equal').addEventListener('click', calculate)
-document.getElementById('#copyToClipboard').addEventListener('click', copyToClipboard)
-document.getElementById('#themeSwitcher').addEventListener('click', switchTheme)
+document.getElementById('clear').addEventListener('click', handleClear)
+document.getElementById('input').addEventListener('keydown', handleTyping)
+document.getElementById('equal').addEventListener('click', calculate)
+document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
+document.getElementById('themeSwitcher').addEventListener('click', switchTheme)
