ff219e77fac9e9f6b9c938916113fdee55d4ffe7
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-10 22:15:49 -0300

    atalhos npx

--- modulo_3/npx_npm_scripts/index.js
+++ modulo_3/npx_npm_scripts/index.js
@@ -1,2 +1,7 @@
 // npx são os executaveis
-//é possível executar os comandos de uma biblioteca dentro do package.json
+//é possível executar os comandos de uma biblioteca dentro dos scripts identificados no arquivo package.json
+//criação de atalhos nos scripts
+
+const helloWorldNpm = require('hello-world-npm')
+
+console.log("Executando hello-world-npm: " + helloWorldNpm())
