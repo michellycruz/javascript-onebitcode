291d1ebdf64606da2017775f88f40f6c94e787f3
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-10 21:48:34 -0300

    Update index.js

--- modulo_3/ex_datas_com_dayjs/index.js
+++ modulo_3/ex_datas_com_dayjs/index.js
@@ -5,9 +5,12 @@ function birthday(date){
     const today = dayjs()
 
     const ageInYears = today.diff(birthday, 'year')
+    const nextBirthday = birthday.add(ageInYears + 1, 'year')
+    const daysToNextBirthday = nextBirthday.diff(today, 'day') + 1
 
     console.log(`Idade: ${ageInYears}`)
+    console.log(`Próximo aniversário: ${nextBirthday.format("DD/MM/YYYY")}`)
+    console.log(`Dias até completar ${ageInYears + 1} anos: ${daysToNextBirthday}`)
 }
-
 
 birthday("1997-12-21")
