75faaee2dabc1721aac578007f8d3e2ae96414aa
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-13 22:01:59 -0300

    Update index.js

--- modulo_3/operador_de_coalescencia_nula/index.js
+++ modulo_3/operador_de_coalescencia_nula/index.js
@@ -3,9 +3,7 @@ const b = null // falso / sem conteúdo, nulo
 const c = "Teste" // verdadeiro / é uma string "contém" um conteúdo
 
 console.log(a || b || c) // o operador lógico || (ou) executará o primeiro valor verdadeiro que encontrar
-
 console.log(b ?? a) // retorna o operando do lado direito quando o seu operador do lado esquerdo é null ou undefined
-
 
 /////////////////////////////////////////////////////////////////////
 
