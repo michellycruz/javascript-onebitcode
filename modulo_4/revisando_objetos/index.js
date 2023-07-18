27372b0d9c223e28931e89a3d5634a06c06b16c3
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-18 16:18:36 -0300

    revisando objetos

--- modulo_4/revisando_objetos/index.js
+++ modulo_4/revisando_objetos/index.js
@@ -1,23 +1,22 @@
-const book = {
-    title: 'Eragon',
-    pages: 468,
-    published: true,
-    inStock: 20,
-    tags: ['Fantasy', 'adventure', 'medieval'],
-    author: {
-        name: 'Christopher Paolini'
-    },
-    addOnStock(quantity){
-        this.inStock += quantity
-    }
-}
+//const book = {
+//    title: 'Eragon',
+//    pages: 468,
+//    published: true,
+//  inStock: 20,
+//    tags: ['Fantasy', 'adventure', 'medieval'],
+//    author: {
+//        name: 'Christopher Paolini'
+//    },
+//    addOnStock(quantity){
+//        this.inStock += quantity
+//    }
+//}
+
+function Book
 
 console.log(book)
-
 book.addOnStock(50)
-
 book.save = function(){
-    //banco de dados
+    //salva no banco de dados
 }
-
-console.log[book]
+console.log(book)
