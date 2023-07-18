b2f275ec5058dc7cd0974772da6f8f5cf428ae7c
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-18 16:29:09 -0300

    Update index.js

--- modulo_4/revisando_objetos/index.js
+++ modulo_4/revisando_objetos/index.js
@@ -2,7 +2,7 @@
 //    title: 'Eragon',
 //    pages: 468,
 //    published: true,
-//  inStock: 20,
+//    inStock: 20,
 //    tags: ['Fantasy', 'adventure', 'medieval'],
 //    author: {
 //        name: 'Christopher Paolini'
@@ -12,11 +12,23 @@
 //    }
 //}
 
-function Book
-
-console.log(book)
-book.addOnStock(50)
-book.save = function(){
-    //salva no banco de dados
+function Book(title, pages, tags, author){
+    this.title = title
+    this.pages = pages
+    this.tags = tags
+    this.author = author
+    this.published = false
+    this.inStock = 0
+    this.addOnStock = function addOnStock (quantity){
+        this.inStock += quantity
+    }
+    this.save = function(){
+        //salva no banco de dados
+    }
 }
-console.log(book)
+
+const author = {name: 'Christopher Paolini'}
+const tags = ['Fantasy', 'adventure', 'medieval']
+const eragon = new Book('Eragon', 468, tags, author) //"new" forma de chamar as funções construtoras
+
+console.log(eragon)
