4eabe393292496543b08a685d02b69dcc35f3901
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-18 22:45:25 -0300

    Create User.js

---
+++ modulo_4/ex_treinando_a_criacao_de_classes/User.js
@@ -0,0 +1,18 @@
+class User {
+    constructor (fullname, email, password){
+        this.fullname = fullname
+        this.email = email
+        this.password = password
+    }
+    login(email, password){
+        if(this.email === email && this.password === password){
+            console.log('Login realizado com sucesso!!')
+        } else{
+            console.log('Falha ao realizar login, email ou senha incorretos')
+        }
+    }
+}
+
+const Maria = new User('Maria do Carmo', "maria@email.com", "1234")
+
+Maria.login('Maria do Carmo', '1234')
