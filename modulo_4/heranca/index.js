5d3128be487082f426a0bc1bab42cc684b78e4c8
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-23 15:52:07 -0300

    heranca

---
+++ modulo_4/heranca/index.js
@@ -0,0 +1,34 @@
+class Property {
+    constructor(area, price){
+        this.area = area
+        this.price = price
+    }
+
+    getPricePerSquareMeter(){
+        return this.price / this.area
+    }
+}
+
+class House extends Property{ }
+
+const land = new Property(200, 50000)
+const someHouse = new House(120, 200000)
+
+console.log(land)
+console.log(someHouse.getPricePerSquareMeter())
+
+class Apartment extends Property{
+    constructor(number, area, price){
+        //this.area = area
+        super(area, price)
+        this.number = number 
+    }
+
+    getFloor(){
+        return this.number.slice(0, -2)
+    }
+}
+
+const apt = new Apartment("201", 100, 160000)
+console.log(apt)
+console.log(apt.getFloor())
