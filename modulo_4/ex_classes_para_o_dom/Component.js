272fc53f48f0d7a12898ed97a0398d612686186f
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-23 18:33:25 -0300

    exercicio

---
+++ modulo_4/ex_classes_para_o_dom/Component.js
@@ -0,0 +1,28 @@
+export class Componet{
+    #element = null
+
+    constructor(tag, parent, options){
+        this.tag = tag
+        this.parent = parent
+        this.options = options
+        this.build()
+    }
+
+    getElement(){
+        return this.#element
+    }
+
+    build(){
+        this.#element = document.createElement(this.tag)
+        Object.assign(this.#element, this.options)
+        return this
+    }
+
+    render(){
+        if (this.parent instanceof Componet){
+            this.parent.getElement().append(this.#element)
+        } else{
+            document.querySelector(this.parent).append(this.#element)
+        }
+    }
+}
