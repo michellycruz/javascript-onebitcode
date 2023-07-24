9b1895abc895d4c862a379f0471c65851ffdb821
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-23 22:08:37 -0300

    Update index.js

--- modulo_4/ex_classes_para_o_dom/index.js
+++ modulo_4/ex_classes_para_o_dom/index.js
@@ -9,3 +9,19 @@ title.render()
 
 title.tag = 'h3'
 title.build().render()
+
+const form = new Form('body')
+const label = new Label('Nome:', form, {htmlFor: 'nameInput'})
+const input = new Input(form, {id: 'nameInput', name: 'name'})
+
+form.render()
+label.render()
+
+form.addChildren(input)
+
+form.addChildren(
+    new Component ('br'),
+    new Component ('br'),
+    new Label('Data de nascimento:', form, {htmlFor: 'birthdayInput'}),
+    new Input(form, {id: 'birthdayInput', name: 'birthday', type: 'date'})
+)
