b1eedc39bc7418f0bbc8d94f8765b8098197f0b8
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-25 11:37:20 -0300

    order

---
+++ modulo_4/js_bookstore/Order.js
@@ -0,0 +1,23 @@
+module.exports = class Order {
+    #total
+    #items
+    #user
+    constructor(item, user){
+        items.forEach(({product, quantity})=>{
+            if(quantity > product.inStock){
+                throw new Error('Quantidade insuficiente em estoque!')
+            }
+        })
+        this.#items = items
+        this.#user = user
+        this.#total = items.reduce((sum, {product, quantity}) => sum + (product.price * quantity), 0)
+    }
+
+    get data(){
+        return{
+            items: this.#items,
+            user: this.#user,
+            total: this.#total
+        }
+    }
+}
