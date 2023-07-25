9bb0abc4f546c0bc5aa2bd3b7f1ab63f2f047d3d
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-25 11:16:49 -0300

    Book

---
+++ modulo_4/js_bookstore/Book.js
@@ -0,0 +1,12 @@
+const Product = require("./Product");
+
+module.exports = class Book extends Product{
+    constructor(title, synopsis, genre, pages, author, description, price, inStock = 0){
+        super(`Livro: ${title}`, description, price, inStock)
+        this.title = title
+        this.synopsis = synopsis
+        this.genre = genre
+        this.pages = pages
+        this.author = author
+    }
+}
