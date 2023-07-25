b2db52b6f2c8bbc6850da6b4e7b185917b17d86a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-25 14:53:29 -0300

    Update App.js

--- modulo_4/js_bookstore/App.js
+++ modulo_4/js_bookstore/App.js
@@ -1,5 +1,7 @@
 const Database = require("./Database")
 const Author = require("./entities/Author")
+const Book = require("./entities/Book")
+const Poster = require("./entities/Poster")
 const User = require("./entities/User")
 
 module.exports = class App{
@@ -21,5 +23,35 @@ module.exports = class App{
 
     getAuthor(){
         return App.#database.find('authors')
+    }
+
+    createBook(title, synopsis, genre, pages, author, description, price, inStock){
+        const book = new Book(title, synopsis, genre, pages, author, description, price, inStock)
+        App.#database.saveBook(book)
+    }
+
+    addBook(bookName, quantity){
+        App.#database.addBooksToStock(bookName, quantity)
+    }
+
+    createPoster(name, description, height, width, price, inStock){
+        const poster = new Poster(name, description, height, width, price, inStock)
+        App.#database.savePoster(poster)
+    }
+
+    addPoster(posterName, quantity){
+        App.#database.addPostersToStock(posterName, quantity)
+    }
+
+    createOrder(items, user){
+        const order = new Order(items, user)
+        App.#database.saveOrder(order)
+        order.data.items.forEach(({product, quantity}) => {
+            if(product instanceof Book){
+                App.#database.removeBooksFromStock(product.name, quantity)
+            } else if (product instanceof Poster){
+                App.#database.removePostersFromStock(product.name, quantity)
+            }
+        })
     }
 }
