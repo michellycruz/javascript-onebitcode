60869ba8cf85bbfc4c2a5b42d85fd4f2c1c023fc
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-26 10:47:09 -0300

    tratamento de erro

---
+++ modulo_5/02_tratamento_de_erros/index.js
@@ -0,0 +1,24 @@
+function sum(a, b){
+    const firstNumber = Number(a)
+    const secondNumber = Number(b)
+
+    if (isNaN(firstNumber) || isNaN(secondNumber)){
+        throw new Error('arguments must be two numbers')
+    }
+
+    return firstNumber + secondNumber
+}
+
+try {
+    console.log(sum(2, 9))
+    console.log(sum(true, 14))
+    //console.log(sum(undefined, 22))
+    console.log(sum(18, "0"))
+    console.log(sum(39, null))
+    //console.log(sum(13, "zero"))
+} catch (error){  // O "try" e o "catch" são usados para lidar com erros e evitar que o programa quebre.
+    console.log("An error ocurred")
+    console.log(error.message)
+} finally {
+    console.log("Calculations finished")
+}
