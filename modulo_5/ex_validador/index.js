07f300d2e6be3d83ab9ebbea21a05c5122318204
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-26 18:36:33 -0300

    exercicio validação de formulário

---
+++ modulo_5/ex_validador/index.js
@@ -0,0 +1,35 @@
+function validateEmail(email){
+    if(!email.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)){ //// Verifica se o email possui 2 caracteres alfanuméricos antes do @, pelo menos 2 letras após o @ e pelo menos 2 letras após o ponto do domínio
+        const err = new Error('Email inválido') //cria um novo objeto "Error"
+        err.input = 'email' //adiciona a propriedade input ao objeto de erro e atribui o valor 'email'
+        throw err // lança o objeto "Error" e interrompe a execução do programa
+    }
+}
+
+function resetFormStyles(inputs){
+    Object.entries(inputs).forEach(([key, value]) => {
+        value.classList.remove('sucess', 'error')
+        document.querySelector(`#${key}-error`).textContent = ''
+    })
+}
+
+const userInputs = {
+    name: document.querySelector('#name'),
+    email: document.querySelector('#email'),
+    password: document.querySelector('#password')
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault() //pra não atualizar a página
+    resetFormStyles(userInputs)
+    try {
+        userInputs.name.classList.add('success')
+        validateEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+    } catch (err) {
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#${err.input}-error`).textContent = err.message
+    }
+})
