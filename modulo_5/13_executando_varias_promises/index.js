6e66df2a85e7c7489590fc3eddc3d9ceafdb25a5
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-27 15:39:26 -0300

    executando várias promises

---
+++ modulo_5/13_executando_varias_promises/index.js
@@ -0,0 +1,19 @@
+function asyncSum(a, b){
+    return new Promise ((resolve, reject) => {
+        if(typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number')
+        } else {
+            resolve(a + b)
+        }
+    })
+}
+
+function asyncSubtract(a, b) {
+    return new Promise((resolve, reject) => {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            reject('arguments must be of type number')
+        } else {
+            resolve(a - b)
+        }
+    })
+}
