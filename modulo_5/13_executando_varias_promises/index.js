86a2e7e4c4a6e8e3f613fdbd49fd91e53a049305
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-27 16:25:13 -0300

    promisse all

--- modulo_5/13_executando_varias_promises/index.js
+++ modulo_5/13_executando_varias_promises/index.js
@@ -17,3 +17,12 @@ function asyncSubtract(a, b) {
         }
     })
 }
+
+const sumResult = asyncSum(50, 33)
+const subtractResult = asyncSubtract(50, 33)
+
+Promise.all([sumResult, subtractResult]).then(results => {
+    console.log(results)
+}).catch(err => {
+    console.log(err)
+})
