9ed2be95cfaa02da649f823b45c78f54fa62681e
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-27 14:25:34 -0300

    then, catch e finally

--- modulo_5/09_utilizando_then_e_catch/index.js
+++ modulo_5/09_utilizando_then_e_catch/index.js
@@ -15,8 +15,8 @@ function execute(){
 
 execute().then((result) => {  // "then" serve para lidar com uma promise resolvida
     console.log(`A promise foi resolvida. O resultado foi: ${result}`)
-}).catch((err) => {
+}).catch((err) => { //"catch" é utilizado para lidar com uma promise que foi rejeitada
     console.log(`A promise foi rejeitada! Motivo: ${err}`)
-}).finally(() => {
+}).finally(() => { // "finally" é opcional e é utilizado para executar uma função de callback.
     console.log('A promisse foi finalizada')
 })
