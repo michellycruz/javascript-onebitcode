7ee8cf56ed5fe0c6db053adfa3bc8b04b4a07ba1
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-28 18:18:12 -0300

    Update index.js

--- modulo_5/23_requisicoes_get/index.js
+++ modulo_5/23_requisicoes_get/index.js
@@ -1,6 +1,23 @@
-async function getCountries(){
-    const require = await fetch('https://restcountries.com/v3.1/all')
+function createCountryCard(country){
+    const card = document.createElement('div')
+    card.classList.add('country')
+
+    const countryName = country.name.common
+    const name = document.createElement('h2')
+    name.textContent = countryName
+
+    const flag = document.createElement('img')
+    flag.src = country.flags.svg
+    flag.alt = countryName
     
+    card.append(name, flag)
+    document.querySelector('#countries').append(card)
+}
+
+async function getCountries(){
+    const response = await fetch('https://restcountries.com/v3.1/all')
+    const countries = await response.json()
+    countries.forEach(createCountryCard)
 }
 
 getCountries()
