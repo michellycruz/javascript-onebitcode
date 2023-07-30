34151996093600f8731935ffc41b2f0894434b66
Mich <125368412+michellycruz@users.noreply.github.com>
2023-07-30 15:50:22 -0300

    JSON

--- modulo_5/24_requisicoes_post/index.js
+++ modulo_5/24_requisicoes_post/index.js
@@ -27,3 +27,22 @@ async function fetchArticles(){
 document.addEventListener('DOMContentLoaded', () => {
     fetchArticles()
 })
+
+const form = document.querySelector('form')
+form.addEventListener('submit', async (ev) => {
+    ev.preventDefault() //para não mudar de página
+
+    const articleData = {
+        title: document.querySelector('#title').value,
+        author: document.querySelector('#author').value,
+        content: document.querySelector('#content').value
+    }
+
+    const response = await fetch('http://localhost:3000/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(articleData)
+    })
+})
