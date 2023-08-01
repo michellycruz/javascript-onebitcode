2b697417795f5f5afdebdde092d43ea24a164264
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-01 20:15:24 -0300

    Update index.js

--- modulo_5/25_ex_consumindo uma api/index.js
+++ modulo_5/25_ex_consumindo uma api/index.js
@@ -5,3 +5,11 @@ function createTransationContainer(id){
     return container
 }
 
+function createTransactionTitle(name){
+    const title = document.createElement('span')
+    title.classList.add('transaction-title')
+    title.textContent = name
+    return title
+}
+
+function
