8bb7b56d51ad89d82092a4aec287d86bda0e781e
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-01 20:26:42 -0300

    formatador de moeda

--- modulo_5/25_ex_consumindo uma api/index.js
+++ modulo_5/25_ex_consumindo uma api/index.js
@@ -12,4 +12,23 @@ function createTransactionTitle(name){
     return title
 }
 
-function
+function createTransactionAmount(amount){
+    const span = documen.createElement('span')
+
+    const formater = Intl.NumberFormat('pt-BR', {
+        compactDisplay: 'long',
+        currency: 'BRL',
+        style: 'currency'
+    })
+    const formatedAmount = formater.format(amount)
+
+    if (amount > 0){
+        span.textContent = `${formatedAmount} C`
+        span.classList.add('credit')
+    } else {
+        span.textContent = `${formatedAmount} D`
+        span.classList.add('debit')
+    }
+
+    return span
+}
