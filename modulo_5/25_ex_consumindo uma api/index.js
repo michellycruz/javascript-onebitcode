136b405a099d8e52fb7e071708cd110ea7c7803e
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-01 21:59:21 -0300

    função delete

--- modulo_5/25_ex_consumindo uma api/index.js
+++ modulo_5/25_ex_consumindo uma api/index.js
@@ -47,13 +47,28 @@ function createEditTransaction(transaction){
     return editBtn
 }
 
+function createDeleteTransactionBtn(id){
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.textContent = 'Excluir'
+    deleteBtn.addEventListener('click', async () => {
+        await fetch(`http://localhost:3000/transactions/${id}`, { method: 'DELETE'})
+        deleteBtn.parentElement.remove()
+        const indexToRemove = transactions.findIndex((t) => t.id === id)
+        transactions.splice(indexToRemove, 1)
+        updateBalance()
+    })
+    return deleteBtn
+}
+
 function renderTransaction(transaction){
     const container = createTransactionContainer(transaction.id)
     const title = createTransactionTitle(transaction.name)
     const amount = createTransactionAmount(transaction.amount)
     const editBtn = createEditTransaction(transaction)
+    const deleteBtn = createDeleteTransactionBtn(transaction.id)
 
-    container.append(title, amount, editBtn)
+    container.append(title, amount, editBtn, deleteBtn)
     document.querySelector('#transactions').append(container)
 }
 
