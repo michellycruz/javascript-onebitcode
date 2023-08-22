d933fa8eacc3077200822df75ea1b64c4219897b
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-22 18:25:07 -0300

    Update index.js

--- exercicios_propostos/informando_dados_ex01/index.js
+++ exercicios_propostos/informando_dados_ex01/index.js
@@ -4,6 +4,9 @@ alert("Bem-vindo! A seguir pediremos que informe alguns dados")
 let nome = prompt("Por favor, informe o seu nome")
 let idade = prompt("Por favor, informe a sua idade")
 
-let confirmacao = confirm("A sua idade é " + idade)
+let confirmacao = confirm(`A sua idade é idade ${idade}?`)
 
-alert("Nome: " + nome + "\nIdade: " + idade + "\nConfirmação de idade: " + confirmacao)
+alert(`
+Nome: ${nome}
+Idade: ${idade}
+Confirmação de idade: ${confirmacao} `)
