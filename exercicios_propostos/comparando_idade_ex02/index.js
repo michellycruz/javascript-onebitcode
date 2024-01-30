adf591a96cce036091e652ca4e27b9ebb07016c6
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-29 21:20:47 -0300

    Update index.js

--- exercicios_propostos/comparando_idade_ex02/index.js
+++ exercicios_propostos/comparando_idade_ex02/index.js
@@ -1,19 +1,30 @@
-alert("Bem vindo ao verificador de difereça de idade")
-let nomePMV = prompt("Por favor, informe o nome da pessoa mais velha")
-let idadePMV = prompt("Por favor informe a idade da pessoa mais velha")
+alert("Verificador de diferença de idade");
 
-let nomePMN = prompt("Por favor, informe o nome da pessoa mais nova")
-let idadePMN = prompt("Por favor, informe a idade da pessoa mais nova")
+let idadePMV = prompt("Informe a idade da pessoa mais velha");
+let nomePMV = prompt
 
-let diferencaIdade = idadePMV - idadePMN
+let PMN = prompt("Informe a idade da pessoa mais nova");
 
-alert(
-`|Pessoa mais velha|
-Nome: ${nomePMV}
-Idade: ${idadePMV}
 
-|Pessoa mais nova|
-Nome: ${nomePMN}
-Idade: ${idadePMN}
-Diferença de idade: ${diferencaIdade} anos
-`)
+
+
+
+// alert("Bem vindo ao verificador de difereça de idade")
+// let nomePMV = prompt("Por favor, informe o nome da pessoa mais velha")
+// let idadePMV = prompt("Por favor informe a idade da pessoa mais velha")
+
+// let nomePMN = prompt("Por favor, informe o nome da pessoa mais nova")
+// let idadePMN = prompt("Por favor, informe a idade da pessoa mais nova")
+
+// let diferencaIdade = idadePMV - idadePMN
+
+// alert(
+// `|Pessoa mais velha|
+// Nome: ${nomePMV}
+// Idade: ${idadePMV}
+
+// |Pessoa mais nova|
+// Nome: ${nomePMN}
+// Idade: ${idadePMN}
+// Diferença de idade: ${diferencaIdade} anos
+// `)
